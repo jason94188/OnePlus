d17f0962f7c897601bda6e8865e0accf7b09baee
jason94188 <2326138805@qq.com>
2018-12-01 17:20:12 +0800

    验证表单更改

--- app/static/js/regist.js
+++ app/static/js/regist.js
@@ -1,39 +1,59 @@
- var move = (function(){
-     var $moveBox = document.querySelector('.moveBox');
-     var $move = $moveBox.querySelector('.move');
-     var maxL = $moveBox.clientWidth - $move.offsetWidth;
-     return {
-         init(){
+var checkInput = {
+    username(str) {
+        var reg = /^\w{6,14}$/;
+        return reg.test(str);
+    },
+    password(str) {
+        var reg = /^\w{6,11}$/;
+        return reg.test(str);
+    },
+    phone(str) {
+        var reg = /^1[35789]\d{9}$/;
+        return reg.test(str);
+    }
+}
+var register = (function(){
+    return {
+        init(ele) {
+            this.$el = document.querySelector(ele);
+            this.$inputAll = this.$el.querySelectorAll('input');
             this.event();
-         },
-         event(){
+            this.$btn = document.querySelector('.btn')
+        },
+        event() {
             var _this = this;
-            var _this = this;
-            $move.onmousedown = function(ev) {
-                ev = ev || window.event;
-                var _x = ev.offsetX;
-                $move.onmousemove = function(ev) {
-                    ev = ev || window.event;
-                    var x = ev.pageX - _x;
-                    _this.moveSlider(x);
+            for(let i = 0; i < this.$inputAll.length; i++) {
+                this.$inputAll[i].onblur = function() {
+                    var $p = this.nextElementSibling;
+                    if(this.value == '') {
+                        $p.className = 'text-danger bg-danger';
+                        $p.innerHTML = '内容不可以为空';
+                    } else {
+                        // 调用对应的方法, 把文本值传入
+                        var bool = checkInput[this.name](this.value);
+                        if(bool) {
+                            // 验证成功
+                            $p.className = 'text-success bg-success';
+                            $p.innerHTML = '验证成功';
+                        } else {
+                            // 验证失败
+                            $p.className = 'text-danger bg-danger';
+                            $p.innerHTML = this.getAttribute('data-error');
+                        }
+                    }
                 }
+            }    
+            this.$btn.onclick = function() {
+                var $pAll = _this.$el.querySelectorAll('p');
+                for(let i = 0; i < $pAll.length; i++) {
+                    if($pAll[i].className.indexOf('text-success') == -1) {
+                        $pAll[i].previousElementSibling.focus();
+                        return;
+                    }
+                }
+                alert('成功');
             }
-         },
-         moveSlider(x){
-            if(x < 0) {
-                x = 0;
-            } else if(x > maxL) {
-                x = maxL;
-                setTimeout(function(){
-                    $move.remove();
-                    // 方便后面做判断
-                    $move = null; 
-                    $moveBox.style.background = 'green';
-                }, 100)
-            }
-            $move.style.left = x + 'px';
-         }
-     }
- }())
-
- move.init()
+        }
+    }
+} ())
+register.init('.form-box');
